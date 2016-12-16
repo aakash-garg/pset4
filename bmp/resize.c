f6534319f62fe42b2a827fba9d261faa2c0f40ae
Aakash Garg <aakash.garg80@gmail.com>
2016-12-16 20:30:11 +0000

    figuring out the basic idea

--- bmp/resize.c
+++ bmp/resize.c
@@ -15,15 +15,22 @@
 int main(int argc, char* argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    if (argc != 4)
     {
         printf("Usage: ./copy infile outfile\n");
         return 1;
     }
+    
+    int n = atoi(argv[1]);
+    if(n<0 || n>100)
+    {
+        printf("the factor should be positive and not more than 100")
+        return 2;
+    }
 
     // remember filenames
-    char* infile = argv[1];
-    char* outfile = argv[2];
+    char* infile = argv[2];
+    char* outfile = argv[3];
 
     // open input file 
     FILE* inptr = fopen(infile, "r");
@@ -45,10 +52,28 @@ int main(int argc, char* argv[])
     // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
-
+    
     // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    int i, j;
+    for(i=0;i<abs(bi.biHeight);i++)
+    {
+        for(j=0;j<bi.biWidth;j++)
+        {
+            /*
+            
+            */
+            RGBTRIPLE triple;
+            fread(&triple,sizeof(RGBTRIPLE),1,inptr)
+            for(int x=0;x<n;x++)
+            {
+                
+            }
+        }
+    }
+    
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
